3138b00ababff00db8552ff2748dc1255caf45be
maskzh <maskzh@gmail.com>
2016-06-30 18:50:36 +0800

    add genId

---
+++ src/_utils.js
@@ -0,0 +1,9 @@
+export function genId(len, prefix, keyspace) {
+  if (len == null) len = 32
+  if (prefix == null) prefix = ''
+  if (keyspace == null) keyspace = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  while (len-- > 0) {
+    prefix += keyspace.charAt(Math.floor(Math.random() * keyspace.length))
+  }
+  return prefix
+}
